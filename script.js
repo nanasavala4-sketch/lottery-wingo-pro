9a9e9570065b30cc49f3adee90f41fd13ecee08b
nanasavala4-sketch <nanasavala4@gmail.com>
2026-07-02 14:12:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  getFirestore,
+  doc,
+  setDoc,
+  getDoc,
+  updateDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7EAtMU0khSVOdcwvRPqiHPq4TYiCusxg",
+  authDomain: "win-color-pro.firebaseapp.com",
+  projectId: "win-color-pro",
+  storageBucket: "win-color-pro.firebasestorage.app",
+  messagingSenderId: "1025777239942",
+  appId: "1:1025777239942:web:6c69f192e6ec7dcb940e07",
+  measurementId: "G-7KXXW0JV0N"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 // ===============================
 // WIN COLOR PRO - Part 1
 // ===============================
