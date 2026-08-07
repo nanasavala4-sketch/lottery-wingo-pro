44f96fcb28c6a38cd909fb682f2dd8899a0dbf64
nanasavala4-sketch <nanasavala4@gmail.com>
2026-08-07 12:39:26 +0530

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,9 @@
+import { db } from "./firebase.js";
+
+import {
+    collection,
+    getDocs,
+    doc,
+    getDoc,
+    updateDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
