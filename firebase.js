1e77f130aef08686dcd7805d8b37fe3d3f2e56ca
nanasavala4-sketch <nanasavala4@gmail.com>
2026-08-08 14:58:08 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,7 +2,9 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
 const firebaseConfig = {
   apiKey: "तुमचा खरा API KEY",
   authDomain: "win-color-pro.firebaseapp.com",
@@ -13,9 +15,7 @@ const firebaseConfig = {
 };
 
 
-
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
 export const db = getFirestore(app);
