c49d6a58d08069e5ae28c26569f412ff615baf2c
nanasavala4-sketch <nanasavala4@gmail.com>
2026-08-08 13:50:01 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,23 @@
-// Firebase SDK
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-// Firebase Config
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD7EAtMU0khSVOdcwvRPqiHPq4TYiCusxg",
-  authDomain: "win-color-pro.firebaseapp.com",
-  projectId: "win-color-pro",
-  storageBucket: "win-color-pro.firebasestorage.app",
-  messagingSenderId: "1025777239942",
-  appId: "1:1025777239942:web:6c69f192e6ec7dcb940e07",
-  measurementId: "G-7KXXW0JV0N"
+    apiKey: "तुमचा सध्याचा apiKey",
+    authDomain: "win-color-pro.firebaseapp.com",
+    projectId: "win-color-pro",
+    storageBucket: "win-color-pro.firebasestorage.app",
+    messagingSenderId: "1025777239942",
+    appId: "तुमचा सध्याचा appId",
+    measurementId: "तुमचा सध्याचा measurementId"
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
