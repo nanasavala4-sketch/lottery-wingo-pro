00d4a328dd3f1f66fc91d068a6a1600c4c070be3
nanasavala4-sketch <nanasavala4@gmail.com>
2026-08-08 15:23:01 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-
 const firebaseConfig = {
-  apiKey: "तुमचा खरा API KEY",
+  apiKey: "AIzaSyD7EAtMU0khSVOdcwvRPqiHPq4TYiCusxg",
   authDomain: "win-color-pro.firebaseapp.com",
   projectId: "win-color-pro",
   storageBucket: "win-color-pro.firebasestorage.app",
-  messagingSenderId: "तुमचा खरा SENDER ID",
-  appId: "तुमचा खरा APP ID"
+  messagingSenderId: "1025777239942",
+  appId: "1:1025777239942:web:6c69f192e6ec7dcb940e07",
+  measurementId: "G-7KXXW0JV0N"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
